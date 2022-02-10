cde1c163ea28d2e04601a4bd65e6682c575da2b0
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-02-10 17:16:26 -0500

    Add Feature: Admin Categories

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,13 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
+  resources :about, only: [:index]
   resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
